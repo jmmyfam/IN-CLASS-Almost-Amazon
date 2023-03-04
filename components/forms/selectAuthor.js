dc6687d94d7f28a329ee2deb7be36db118c8d4ee
jmmyfam <pham_j@ymail.com>
2023-03-04 10:01:33 -0600

    cleaned and fixed author drop down list

--- components/forms/selectAuthor.js
+++ components/forms/selectAuthor.js
@@ -6,7 +6,7 @@ const selectAuthor = (user, authorId) => {
     <select class="form-control" id="author_id" required>
     <option value="">Select an Author</option>`;
 
-  getAuthors().then((authorsArray) => {
+  getAuthors(user.uid).then((authorsArray) => {
     authorsArray.forEach((author) => {
       domString += `
           <option 
